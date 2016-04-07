d42b5ea6653b1efa9da9eecd5f6d900645c9a4fc
Mdo Cissé <mamadou.cisse@recia.fr>
2016-04-07 16:33:17 +0200

    mise en commentaire d'une exception

--- cache/classes/helper.php
+++ cache/classes/helper.php
@@ -527,7 +527,13 @@ class cache_helper {
     public static function hash_key($key, cache_definition $definition) {
         if ($definition->uses_simple_keys()) {
             if (debugging() && preg_match('#[^a-zA-Z0-9_]#', $key)) {
-                throw new coding_exception('Cache definition '.$definition->get_id().' requires simple keys. Invalid key provided.', $key);
+                ////////////////////////////////////////////////
+                // MODIFICATION RECIA | DEBUT | 2013-07-08
+                ////////////////////////////////////////////////
+                // throw new coding_exception('Cache definition '.$definition->get_id().' requires simple keys. Invalid key provided.', $key);
+                ////////////////////////////////////////////////
+                // MODIFICATION RECIA | FIN
+                ////////////////////////////////////////////////
             }
             // We put the key first so that we can be sure the start of the key changes.
             return (string)$key . '-' . $definition->generate_single_key_prefix();
