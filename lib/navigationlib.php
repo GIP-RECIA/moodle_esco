d9d2cc2a7954453d2939a4bddcdb4555fdec2a53
Mdo Cissé <mamadou.cisse@recia.fr>
2016-04-08 14:15:27 +0200

    ajout possibilité de suppression d'un cours dans la barre de navigation

--- lib/navigationlib.php
+++ lib/navigationlib.php
@@ -3955,6 +3955,18 @@ class settings_navigation extends navigation_node {
             $url = new moodle_url('/course/reset.php', array('id'=>$course->id));
             $coursenode->add(get_string('reset'), $url, self::TYPE_SETTING, null, null, new pix_icon('i/return', ''));
         }
+                
+        ////////////////////////////////////////////////
+        // MODIFICATION RECIA | DEBUT | 2013-06-27
+        ////////////////////////////////////////////////
+        // Delete this course
+        if (has_capability('moodle/course:delete', $coursecontext)) {
+            $url = new moodle_url('/course/delete-for-user.php', array('id'=>$course->id));
+            $coursenode->add(get_string('delete'), $url, self::TYPE_SETTING, null, null, new pix_icon('t/delete', ''));
+        }
+        ////////////////////////////////////////////////
+        // MODIFICATION RECIA | FIN
+        ////////////////////////////////////////////////
 
         // Questions
         require_once($CFG->libdir . '/questionlib.php');
